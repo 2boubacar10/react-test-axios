a3f87e77970643a71d68126eca855cc4810fe4f3
Boubacar Diallo <dalandafereya@gmail.com>
2022-03-24 09:25:09 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class PersonList extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      lastname: this.state.name,
+      profile_type: "professionnel"
+      //_token : document.getElementsByName("_token")[0].value
+    };
+
+    const description = "Description";
+    const name = "Boubacar";
+    const offer_type = "Boubacar";
+
+    const offer = {
+      description: "Le champ description est obligatoire.",
+      name: "Le champ name est obligatoire."
+    };
+
+    axios.get('http://localhost:8000/sanctum/csrf-cookie')
+    .then(response => {
+      console.log(response);
+        axios.post(`http://localhost:8000/api/v1/login`, { name, description, offer_type })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+    });
+
+    /*axios.post(`http://localhost:8000/api/v1/subscriptions`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })*/
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
 }
-
-export default App;
